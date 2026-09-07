777415ee67abd48793cbdcf15a4522c31b24fdeb
Damigu Djagbare <djagbare2d@gmail.com>
2026-09-07 09:42:04 -0600

    toLowerCase for client code

--- app/login/route.ts
+++ app/login/route.ts
@@ -23,7 +23,7 @@ export async function POST(request:NextRequest) {
         if (!user || user===null) return NextResponse.json({message: "Utilisateur non trouvé"}, { status: 404 });
         const userClient = await getUserClient(user.id);
         if (!userClient || userClient===null) return NextResponse.json({message: "Utilisateur non associé à un client"}, { status: 404 });
-        if (loginRequest.clientCode.toUpperCase() !== userClient.code) return NextResponse.json({message: "Utilisateur non associé au client"}, { status: 404 });
+        if (loginRequest.clientCode.toLowerCase() !== userClient.code) return NextResponse.json({message: "Utilisateur non associé au client"}, { status: 404 });
         const userRoles = await getUserRoles(user.id);
         const userResources = await getUserResources(user.id);
         let menuItems: SagesMenuItem[] = [];
