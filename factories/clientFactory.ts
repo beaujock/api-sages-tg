857c52e9093b73510acb2947126fb8691f940fd2
Damigu Djagbare <djagbare2d@gmail.com>
2026-09-07 09:45:15 -0600

    login process 02

--- factories/clientFactory.ts
+++ factories/clientFactory.ts
@@ -104,7 +104,7 @@ export async function getClientByCode(clientCode:string) : Promise<AdminClientCl
         if ( !isConnected ) throw new Error("Vous n'êtes pas connecté!");
         const client = await prisma.sgs_client.findFirst({
             where : {
-                code : clientCode.toUpperCase()
+                code : clientCode.toLocaleLowerCase()
             },
             include : {
                 tg_systeme_scolaire : true,
