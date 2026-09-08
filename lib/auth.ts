9614d8e29943691eb24efb2441ec44e0139f7895
Damigu Djagbare <djagbare2d@gmail.com>
2026-09-07 21:01:08 -0600

    auth works

--- lib/auth.ts
+++ lib/auth.ts
@@ -64,7 +64,8 @@ export async function getConnectedUser(req: NextRequest) : Promise<sgs_user|null
     if (!authHeader || !authHeader.startsWith('Bearer ')) throw new Error("Authorization header missing or malformed");
     const token = authHeader.split(' ')[1];
 
-    const { payload: decodedToken } = await jwtVerify(token, secretKey, { clockTolerance: 60 });
+    const { payload: decodedToken } = await verifyToken(token) as { payload: JWTPayload };
+    if (!decodedToken || !decodedToken.user_id) throw new Error("Invalid token payload");
     
     // Type casting the user id from the payload
     const userId = decodedToken.user_id as string
