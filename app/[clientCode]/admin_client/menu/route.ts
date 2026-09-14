f8dc7933210e1566db8da0355692a435e3f0a0ce
Damigu DJAGBARE <djagbare2d@gmail.com>
2026-09-14 14:50:51 -0600

    clean up

--- app/[clientCode]/admin_client/menu/route.ts
+++ app/[clientCode]/admin_client/menu/route.ts
@@ -1,14 +1,19 @@
 import { getClientRoleMenuItems } from "@/factories/clientFactory";
 import { logError } from "@/factories/utilitiesFactory";
+import { getClientUserRouteRequestInfos } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function GET(request:NextRequest, { params }: { params: Promise<{clientCode:string }> }) {
     try {
-        const clientCode = (await params).clientCode;
-        if(!clientCode) return NextResponse.json("Requête invalide (identification du code client)", { status: 400 });
-        const userClientRoleMenuItems = await getClientRoleMenuItems(clientCode, "ADMIN_CLIENT");
-        return NextResponse.json({menuItems : userClientRoleMenuItems}, { status: 200 });
+            const clientCode = (await params).clientCode;
+            if(!clientCode) return NextResponse.json({message : "Requête invalide (code client manquant)"}, { status: 400 });
+            const requestedRouteInfos = await getClientUserRouteRequestInfos(request, clientCode, "ADMIN_CLIENT","CLIENT");
+            if (requestedRouteInfos.client === null || requestedRouteInfos.user === null || !requestedRouteInfos.allowed || requestedRouteInfos.resources.length === 0)
+                return NextResponse.json({message : requestedRouteInfos.message}, { status: 400 });
+            const client = requestedRouteInfos.client;
+            const userClientRoleMenuItems = await getClientRoleMenuItems(clientCode, "ADMIN_CLIENT");
+            return NextResponse.json({menuItems : userClientRoleMenuItems}, { status: 200 });
     }
     catch(error:any) {
         logError('F',"Echec : Menu d'un utilisateur",(new URL(request.url)).pathname, error.message, true);
