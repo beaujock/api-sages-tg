7c725d54e65b4c1bf48a60dc6030aeeff85ad68e
Damigu Djagbare <djagbare2d@gmail.com>
2026-09-15 14:32:08 -0600

    remove ecole is from menu links and actions

--- app/[clientCode]/admin_client/ecoles/actions/route.ts
+++ app/[clientCode]/admin_client/ecoles/actions/route.ts
@@ -5,12 +5,10 @@ import { getRoleByCode } from "@/factories/clientFactory";
 import { getClientMenuItemActions } from "@/factories/ADMIN_CLIENT/clientFactory";
 
 
-export async function GET(request:NextRequest, { params }: { params: Promise<{clientCode: string, ecoleId: string}> }) {
+export async function GET(request:NextRequest, { params }: { params: Promise<{clientCode: string}> }) {
     try {
         const clientCode = (await params).clientCode;
         if(!clientCode) return NextResponse.json({message : "Requête invalide (code client manquant)"}, { status: 400 });
-        const ecoleId = (await params).ecoleId;
-        if(!ecoleId) return NextResponse.json({message : "Requête invalide (Identifcation de l'école manquant)"}, { status: 400 });
         const requestedRouteInfos = await getClientUserRouteRequestInfos(request, clientCode, "ADMIN_CLIENT","CLIENT");
         if (requestedRouteInfos.client === null || requestedRouteInfos.user === null || !requestedRouteInfos.allowed || requestedRouteInfos.resources.length === 0)
             return NextResponse.json({message : requestedRouteInfos.message}, { status: 400 });
