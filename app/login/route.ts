e84213dc58e532282f43eb51eb93f06edd4182a5
Damigu DJAGBARE <djagbare2d@gmail.com>
2026-09-21 09:39:18 -0600

    add roles to token

--- app/login/route.ts
+++ app/login/route.ts
@@ -38,6 +38,7 @@ export async function POST(request:NextRequest) {
         const connectionToken = await generateToken({
             "user_id"            : user.id,
             "user_full_name"     : user.user_full_name,
+            "roles"              : userRoles,
             "effective_date"     : effective_date_time,
             "expiry_date"        : expiry_date_time,
             "user_ip_address"    : request.headers.get('x-forwarded-for') || null,
