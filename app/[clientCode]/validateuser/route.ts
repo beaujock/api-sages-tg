660418cddc758fc84459be685480085713299cfc
Damigu DJAGBARE <djagbare2d@gmail.com>
2026-09-23 10:56:53 -0600

    validateuser

---
+++ app/[clientCode]/validateuser/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { logError } from "@/factories/utilitiesFactory";
+import { getClientUserRouteRequestInfos, verifyToken } from "@/lib/auth";
+import { isWithinInterval } from 'date-fns';
+import { SagesToken } from "@/types/ALL_USAGE/AllUsagesTypes";
+import { getUserResources, getUserRoles } from "@/factories/userFactory";
+import { SagesMenuItem } from "@/types/USERX/UserTypes";
+import { getClientRoleMenuItems } from "@/factories/clientFactory";
+
+export async function GET(request:NextRequest, { params }: { params: Promise<{clientCode: string}> }) {
+    try {
+
+        const clientCode = (await params).clientCode;
+        if(!clientCode) return NextResponse.json({message : "Requête invalide (code client manquant)"}, { status: 400 });
+        const requestedRouteInfos = await getClientUserRouteRequestInfos(request, clientCode, "ADMIN_CLIENT","CLIENT");
+        if (requestedRouteInfos.client === null || requestedRouteInfos.user === null || !requestedRouteInfos.allowed || requestedRouteInfos.resources.length === 0)
+            return NextResponse.json({message : requestedRouteInfos.message}, { status: 400 });
+        const user = requestedRouteInfos.user;
+        const client = requestedRouteInfos.client;
+        const userRoles = await getUserRoles(user.id);
+        const userResources = await getUserResources(user.id);
+        let menuItems: SagesMenuItem[] = [];
+        if (userRoles && userRoles.length === 1) {
+            const roleCode = userRoles[0];
+            menuItems = await getClientRoleMenuItems(client.code.toLowerCase(), roleCode.toUpperCase());
+        };
+
+        //request.headers.set("Access-Control-Allow-Origin", "*");
+        return NextResponse.json({"user_id" : user.id,  first_login : user.first_login, roles : userRoles}, 
+            { status: 200 });
+    }
+    catch(error:any){
+        logError('F',"Vérification de jeton",(new URL(request.url)).pathname, error.message, true);
+        return NextResponse.json({message : error.message}, { status: 500 });
+    }
+}
