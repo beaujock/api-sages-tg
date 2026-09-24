291f1b9f85ccf5317dd8edc39de1ce5878e1469b
Damigu Djagbare <djagbare2d@gmail.com>
2026-09-24 15:03:27 -0600

    getElevePhoto is a POST

--- app/[clientCode]/admin_client/eleves/getelevephoto/route.ts
+++ app/[clientCode]/admin_client/eleves/getelevephoto/route.ts
@@ -6,7 +6,7 @@ import { getClientMenuItemLinks } from "@/factories/ADMIN_CLIENT/clientFactory";
 import { getElevePhotoUrl } from "@/factories/ALL_USAGE/allUsageFactories";
 
 
-export async function GET(request:NextRequest, { params }: { params: Promise<{clientCode: string}> }) {
+export async function POST(request:NextRequest, { params }: { params: Promise<{clientCode: string}> }) {
     try {
         const clientCode = (await params).clientCode;
         if(!clientCode) return NextResponse.json({message : "Requête invalide (code client manquant)"}, { status: 400 });
